02c01576c6f3e7db2bab4c13cf5906b72b3e164e
egi.ramadhani <321910004@student.machung.ac.id>
2024-08-09 21:25:31 +0700

    Add: GetAllEmployee

---
+++ hrsi-api/hrsi-api/Controllers/Master/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using hrsi_api.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hrsi_api.Controllers.Master
+{
+    [Route("api/[controller]")]
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public EmployeeController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAllEmployee ()
+        {
+            var allEmployee = dbContext.employee.ToList();
+
+            return Ok(allEmployee);
+        }
+    }
+}
