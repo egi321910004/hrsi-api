6f9d747a9eae67370faff99606923e8a731f1c4c
egi.ramadhani <321910004@student.machung.ac.id>
2024-08-10 21:56:05 +0700

    Add: InsertEmployeeDTO

---
+++ hrsi-api/hrsi-api/DTO/InsertEmployeeDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace hrsi_api.DTO
+{
+    public class InsertEmployeeDTO
+    {
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Phone { get; set; }
+        public required string status { get; set; }
+    }
+}
