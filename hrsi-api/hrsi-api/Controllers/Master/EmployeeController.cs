4119488f0a4eff4bc0f61360d303ef6f96eebfab
egi.ramadhani <321910004@student.machung.ac.id>
2024-08-10 22:04:10 +0700

    Add: InsertEmployee

--- hrsi-api/hrsi-api/Controllers/Master/EmployeeController.cs
+++ hrsi-api/hrsi-api/Controllers/Master/EmployeeController.cs
@@ -1,4 +1,7 @@
 ﻿using hrsi_api.Data;
+using hrsi_api.DTO;
+using hrsi_api.Migrations;
+using hrsi_api.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace hrsi_api.Controllers.Master
@@ -13,11 +16,33 @@ namespace hrsi_api.Controllers.Master
             this.dbContext = dbContext;
         }
         [HttpGet]
-        public IActionResult GetAllEmployee ()
+        public IActionResult GetAllEmployee()
         {
             var allEmployee = dbContext.employee.ToList();
 
             return Ok(allEmployee);
+        }
+
+        [HttpPost]
+        public IActionResult InsertEmployee([FromBody] InsertEmployeeDTO addEmployeeDto)
+        {
+            if (addEmployeeDto == null)
+            {
+                return BadRequest("Employee data is null");
+            }
+
+            var employeeData = new Employee()
+            {
+                Name = addEmployeeDto.Name,
+                Email = addEmployeeDto.Email,
+                Phone = addEmployeeDto.Phone,
+                status = addEmployeeDto.status 
+            };
+
+            dbContext.employee.Add(employeeData);
+            dbContext.SaveChanges();
+
+            return Ok(employeeData);
         }
     }
 }
