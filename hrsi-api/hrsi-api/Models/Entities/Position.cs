b11fed6e388bef1dc450ae3fcd63aa143c15b799
egi.ramadhani <321910004@student.machung.ac.id>
2024-08-11 19:14:49 +0700

    Add: Position Models

---
+++ hrsi-api/hrsi-api/Models/Entities/Position.cs
@@ -0,0 +1,12 @@
+﻿namespace hrsi_api.Models.Entities
+{
+    public class Position
+    {
+        public Guid Id { get; set; }
+        public required string name_position { get; set; }
+        public required string salary { get; set; }
+        public required string division { get; set; }
+        public required string department { get; set; }
+        public required string status { get; set; }
+    }
+}
