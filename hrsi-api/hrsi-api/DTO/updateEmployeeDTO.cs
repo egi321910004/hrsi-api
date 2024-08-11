35e5151697eaad595d4603b8b30b9a12bb612594
egi.ramadhani <321910004@student.machung.ac.id>
2024-08-11 21:58:36 +0700

    Update: Add Position ID

--- hrsi-api/hrsi-api/DTO/updateEmployeeDTO.cs
+++ hrsi-api/hrsi-api/DTO/updateEmployeeDTO.cs
@@ -2,6 +2,7 @@
 {
     public class updateEmployeeDTO
     {
+        public required Guid PositionId { get; set; }
         public required string Name { get; set; }
         public required string Email { get; set; }
         public required string Phone { get; set; }
