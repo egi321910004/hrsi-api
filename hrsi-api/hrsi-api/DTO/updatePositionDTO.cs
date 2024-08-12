d3dcdb81db2d399cd5acd36436a9d56267e91d68
egi.ramadhani <321910004@student.machung.ac.id>
2024-08-12 09:22:11 +0700

    Add: updatePositionDTO

---
+++ hrsi-api/hrsi-api/DTO/updatePositionDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace hrsi_api.DTO
+{
+    public class updatePositionDTO
+    {
+        public required string name_position { get; set; }
+        public required string salary { get; set; }
+        public required string division { get; set; }
+        public required string department { get; set; }
+        public required string status { get; set; }
+    }
+}
